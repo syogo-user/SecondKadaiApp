48d79531f02ac1bfc06dbd95d4aa18b1e8022da8
syogo-user <s.photo.trumpet.005@gmail.com>
2019-12-31 16:08:29 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by 小野寺祥吾 on 2019/12/31.
+//  Copyright © 2019 syogo-user. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
